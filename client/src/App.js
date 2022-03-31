575f10963184fe3c401f23908c44aa0363794381
gammahazard <92896466+gammahazard@users.noreply.github.com>
2022-03-30 17:55:13 -0700

    login/logout button + homepage welcome msg
    added welcome text

--- client/src/App.js
+++ client/src/App.js
@@ -3,20 +3,25 @@ import React from "react";
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./component/navbar/Navbar";
-
+import Login from "./component/Login";
 import Footer from "./component/footer/Footer";
 import Faq from "./component/footer/Faq";
-import Login from "./component/Login";
-
+import HomePage from "./component/Homepage";
+import Logout from "./component/Logout";
+import Profile from "./Profile";
 function App() {
   return (
     <div>
       <Router basename={process.env.PUBLIC_URL}>
         <Navbar />
+        <HomePage />
         <Routes>
           <Route exact path="/faq" element={<Faq />} />
-          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/profile" element={<Login />} />
         </Routes>
+<Logout />
+<Login />
+<Profile />
         <Footer />
       </Router>
     </div>
