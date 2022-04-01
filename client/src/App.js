475cf1f5f87146bf85be3bffa0e683d05dea47cd
gammahazard <92896466+gammahazard@users.noreply.github.com>
2022-03-31 23:13:39 -0400

    fix?

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,14 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./component/navbar/Navbar";
 
 import Footer from "./component/footer/Footer";
+
 import Faq from "./component/footer/Faq";
 import HomePage from "./component/Homepage";
+
+
+// import Faq from "./component/footer/FAQ";
+import Login from "./component/Login";
+
 
 import Profile from "./component/Profile";
 function App() {
@@ -20,7 +26,10 @@ function App() {
      
         </Routes>
 
-<Profile />
+
+
+        <Profile />
+
         <Footer />
       </Router>
     </div>
