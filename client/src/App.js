d14d1d09875faa46950d97ccba17148ffea3219d
Darimo4ka <darima_badm@yahoo.com>
2022-04-01 11:06:45 -0400

    faq problems

--- client/src/App.js
+++ client/src/App.js
@@ -5,9 +5,9 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./component/navbar/Navbar";
 
 import Footer from "./component/footer/Footer";
-// import Faq from "./component/footer/FAQ";
+import Faq from "./component/footer/Faq";
 import Login from "./component/Login";
-import FAQ from "./component/footer/FAQ";
+
 import Contact from "./component/footer/Contact";
 function App() {
   return (
@@ -15,9 +15,10 @@ function App() {
       <Router basename={process.env.PUBLIC_URL}>
         <Navbar />
         <Routes>
-          <Route exact path="/faq" element={<FAQ />} />
+          <Route exact path="/faq" element={<Faq />} />
           <Route exact path="/login" element={<Login />} />
           <Route exact path="/contact" element={<Contact />} />
+  
         </Routes>
         <Footer />
       </Router>
