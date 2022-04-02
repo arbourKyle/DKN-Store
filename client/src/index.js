2a40be7ea3f0bc9ce5ab374b1f4bee974e3222fb
gammahazard <92896466+gammahazard@users.noreply.github.com>
2022-04-02 13:40:42 -0700

    apolloclient for queries
    added to index.js, added apolloclient pckg

--- client/src/index.js
+++ client/src/index.js
@@ -5,15 +5,26 @@ import { Auth0Provider } from "@auth0/auth0-react";
 import "jquery";
 import "bootstrap/dist/js/bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
-
-
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql
+} from "@apollo/client";
+const client = new ApolloClient({
+  uri: 'http://localhost:3001/graphql',
+  cache: new InMemoryCache()
+});
 ReactDOM.render(
   <Auth0Provider
     domain="dev-xnlw15h5.us.auth0.com"
     clientId="ljAacqxUfHFmCdK9jjRJCr6TMlMqGlkK"
     redirectUri={window.location.origin}
   >
+    <ApolloProvider client={client} >
     <App />
+  </ApolloProvider >
   </Auth0Provider>,
   
   document.getElementById("root")
