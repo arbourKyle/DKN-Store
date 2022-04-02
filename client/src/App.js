addb86314123a353e68455648f2268d1cbfb9918
KyleA <arbourkyle@gmail.com>
2022-04-02 00:07:33 -0400

    redoing previous conflicting changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,26 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-// <<<<<<< kyleFrontEndBranch
-// import './App.css';
-// =======
-// import Navbar from "./component/navbar/Navbar";
+
+import './App.css';
+
+
+
 
 // import Footer from "./component/footer/Footer";
-
 // import Faq from "./component/footer/Faq";
-// import Login from "./component/Login";
-
-
-
 // import Faq from "./component/footer/Faq";
-// import HomePage from "./component/Homepage";
 
-// >>>>>>> main
 
+ // import Login from "./component/Login";
+ // import HomePage from "./component/Homepage";
 
 //dashboard imports
 
   // import Dashboard from "./component/dashboard";
   // import Login from "./component/Login";
 
-//navbar imports
+
+  //navbar imports
 
   import Navbar from "./component/testComponents/Navbar";
   // import Navbar from "./component/navbar/Navbar";
@@ -44,43 +41,18 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 
 
-// <<<<<<< kyleFrontEndBranch
-// function App() {
-//   return (
-//     <>
+
+function App() {
+  return (
+    <>
     
-//     <Navbar />
-//     <Background />
-//     <Card />
+    <Navbar />
+    <Background />
+    <Card />
 
     
-//     </>
-// =======
+    </>
 
-// import Contact from "./component/footer/Contact";
-
-// function App() {
-//   return (
-//     <div>
-//       <Router basename={process.env.PUBLIC_URL}>
-//         <Navbar />
-//         <Routes>
-//           <Route exact path="/faq" element={<Faq />} />
-//           <Route exact path="/login" element={<Login />} />
-//           <Route exact path="/contact" element={<Contact />} />
-  
-//         </Routes>
-
-
-
-
-//         <Dashboard />
-     
-
-//         <Footer />
-//       </Router>
-//     </div>
-// >>>>>>> main
   );
 }
 
