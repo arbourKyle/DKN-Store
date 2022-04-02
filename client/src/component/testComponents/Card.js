b7d25911ce5f980e9f165de4aaee3f2439285d05
gammahazard <92896466+gammahazard@users.noreply.github.com>
2022-04-02 14:02:36 -0700

    query working
    edit query to include fields needed, need to filter through to display products because it looks bad

--- client/src/component/testComponents/Card.js
+++ client/src/component/testComponents/Card.js
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import ProductInfo from '../../index';
 const Card = () => {
 	return (
 <div class="row row-cols-1 row-cols-md-2 g-4">
-  <div class="col">
+  <div class="col"> {ProductInfo()}
     <div class="card">
       <img src="..." class="card-img-top" alt="..."></img>
       <div class="card-body">
--- client/src/index.js
+++ client/src/index.js
@@ -16,6 +16,30 @@ const client = new ApolloClient({
   uri: 'http://localhost:3001/graphql',
   cache: new InMemoryCache()
 });
+const PRODUCT_INFO = gql `query Product {
+  products {
+  
+    name
+   
+    units
+  }
+}`
+
+export default function ProductInfo () {
+  const { loading, error, data } = useQuery(PRODUCT_INFO)
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return data.products.map(({ name, units }) => (
+    <div>
+      <p>
+        {name}:  {units}: 
+     
+      </p>
+    </div>
+  ));
+}
+
 ReactDOM.render(
   <Auth0Provider
     domain="dev-xnlw15h5.us.auth0.com"
