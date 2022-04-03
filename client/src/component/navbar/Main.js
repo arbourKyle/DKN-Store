855cecff5f1f497ca725478ff130027011962aa1
gammahazard <92896466+gammahazard@users.noreply.github.com>
2022-04-03 02:20:05 -0700

    typewriter stuff

--- client/src/component/navbar/Main.js
+++ client/src/component/navbar/Main.js
@@ -1,13 +1,16 @@
 import React from "react";
 import "./Main.css"
 import Login from "../dashboard/Login";
+
+
 const Main = () => {
     return ( 
  
 
-            <div className="scale-in-center" id="welcome"><h1 id="welcometext"> WELCOME</h1>
-            <p>Please Login or Sign-up to continue!</p>
+            <div className="scale-in-center" id="welcome"><h1 className="line-1 anim-typewriter" id="welcometext"> Welcome to (nameofshop)</h1><br></br>
+            <p className="line-1 anim-typewriter">Please Login or Sign-up to continue!</p>
             <Login />
+         
             </div>
 
 
