71566bd513ac1f57ab67925bb9becf5e1f5dcbd1
gammahazard <92896466+gammahazard@users.noreply.github.com>
2022-04-04 01:29:44 -0400

    styling login

--- client/src/component/dashboard/Login.js
+++ client/src/component/dashboard/Login.js
@@ -6,7 +6,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 const Login = () => {
   const { loginWithRedirect } = useAuth0();
 
-  return  <button onClick={() => loginWithRedirect()}>Log In/Sign up</button>;
+  return  <button id="loginhomebtn" onClick={() => loginWithRedirect()}>Enter<br></br> the<br></br> shop!</button>;
  
 }
 
