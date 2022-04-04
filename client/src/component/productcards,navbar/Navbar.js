04110326044fc84a8f75bb8e9c9da1e8139518eb
gammahazard <92896466+gammahazard@users.noreply.github.com>
2022-04-04 00:26:58 -0400

    styling

--- client/src/component/productcards,navbar/Navbar.js
+++ client/src/component/productcards,navbar/Navbar.js
@@ -23,7 +23,7 @@ const Navbar = () =>{
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            More Info
+            
           </a>
           <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
   
