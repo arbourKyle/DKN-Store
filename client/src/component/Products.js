b5f6f5dd7fbffd7285684cc4a90a755c52bcd7d4
gammahazard <92896466+gammahazard@users.noreply.github.com>
2022-04-03 20:09:53 -0400

    createproductfix
    something happens when trying to log in on different browsers under same authentication token, causes memory leak?

--- client/src/component/Products.js
+++ client/src/component/Products.js
@@ -1,11 +1,12 @@
 import "../component/products.css"
+import CreateProduct from "./createproduct";
 const Products = () => {
     return (
       <div id="producttext">
   
    <h4 className="line-1 anim-typewriter">Thanks for signing up, check out our products below!</h4>
       
-            
+            <CreateProduct />
         
  
   
---
+++ client/src/component/createproduct.js
@@ -0,0 +1,123 @@
+import React, { useState } from 'react';
+import { useMutation, gql } from '@apollo/client';
+
+const CREATE_LINK_MUTATION = gql`
+mutation Mutation($name: String!, $image: String!, $category: String!) {
+  createProduct(name: $name, image: $image, category: $category) {
+    name
+    image
+    category {
+      name
+    }
+  }
+}`
+const CreateProduct = () => {
+  const [formState, setFormState] = useState({
+    name: '' ,
+    image: '' ,
+    category: '' ,
+   short_desc: '' ,
+    price: '' 
+
+  })
+
+  const [CreateProduct] = useMutation(CREATE_LINK_MUTATION, {
+    variables: {
+      name: formState.name,
+      image: formState.image,
+      category: formState.category,
+      short_desc: formState.short_desc,
+      price: formState.price
+
+    }
+  });
+
+  return (
+    <div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          CreateProduct();
+          console.log('product created')
+          console.log('name:', formState.name)
+          console.log('image-path', formState.image)
+          console.log('category:', formState.category)
+          console.log('short_description:', formState.short_desc)
+          console.log('price:', formState.price)
+        }}
+      >
+        <div className="flex flex-column mt3">
+          <input
+            className="mb2"
+            value={formState.name}
+            onChange={(e) =>
+              setFormState({
+                ...formState,
+                name: e.target.value
+              })
+            }
+            type="string"
+            placeholder="Name of product"
+          />
+
+                    <input
+            className="mb2"
+            value={formState.image}
+            onChange={(e) =>
+              setFormState({
+                ...formState,
+                image: e.target.value
+              }) 
+            }
+            
+            type="string"
+            placeholder="specify img path"
+          />
+          <input
+            className="mb2"
+            value={formState.category}
+            onChange={(e) =>
+              setFormState({
+                ...formState,
+                category: e.target.value
+              }) 
+            }
+            
+            type="string"
+            placeholder="specify category"
+          />
+                    <input
+            className="mb2"
+            value={formState.short_desc}
+            onChange={(e) =>
+              setFormState({
+                ...formState,
+                short_desc: e.target.value
+              }) 
+            }
+            
+            type="string"
+            placeholder="specify short description"
+          />
+
+          <input
+            className="mb2"
+            value={formState.price}
+            onChange={(e) =>
+              setFormState({
+                ...formState,
+                price: e.target.value
+              }) 
+            }
+            
+            type="int"
+            placeholder="specify price"
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default CreateProduct;
