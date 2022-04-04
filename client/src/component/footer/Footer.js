dabff97f55e92a8997210e21103db8639b9f4ba4
gammahazard <92896466+gammahazard@users.noreply.github.com>
2022-04-04 00:40:45 -0400

    styling

--- client/src/component/footer/Footer.js
+++ client/src/component/footer/Footer.js
@@ -9,8 +9,8 @@ const Footer = () => {
   return (
 
       <span>
-     <p> <footer className="footer"> <p id="ouraddress">Our Address</p><p> Main street, ON</p>
-         <p id="phonenum"> Our Phone number: 416-000-0000 </p>
+     <p> <footer className="footer"> <p id="ouraddress">Our Address<br></br> Main street west, Toronto Ontario</p>
+         <p id="ouraddress"> Our Phone number<br></br> 416-000-0000 </p>
          </footer></p> 
       </span>
  
